7f71aa7f65e9467d0d9d5bbfab07f2d8a039778a
Ssunsito <santiago.pila@epn.edu.ec>
2025-05-01 15:37:51 -0500

    Update script.js
    Cambio 1

--- 1_Fundamentos/script.js
+++ 1_Fundamentos/script.js
@@ -1,9 +1,10 @@
 // Vector de restaurantes
 const restaurantes = [
-  { nombre: "Restaurante El Buen Sabor", tipo: "Tradicional", horario: "12:00 - 22:00" },
-  { nombre: "Café del Valle", tipo: "Cafetería", horario: "08:00 - 20:00" },
-  { nombre: "La Parrilla de San José", tipo: "Parrillada", horario: "12:00 - 23:00" },
-  { nombre: "Tortillas tradicionales", tipo: "Tradicinal", horario: "13:00 - 21:00" }
+  { nombre: "Restaurante El Buen Sabor", tipo: "Tradicional", horario: "12:00 - 22:00", reputacion: 1},
+  { nombre: "Café del Valle", tipo: "Cafetería", horario: "08:00 - 20:00", reputacion: 2 },
+  { nombre: "La Parrilla de San José", tipo: "Parrillada", horario: "12:00 - 23:00", reputacion: 3 },
+  { nombre: "Tortillas tradicionales", tipo: "Tradicinal", horario: "13:00 - 21:00", reputacion: 4 },
+  { nombre: "Dulcería Buen Sabor", tipo: "Dulcería", horario: "08:00 - 18:00", reputacion: 5 },
 ];
 
 // Función para renderizar la lista de restaurantes
@@ -23,11 +24,15 @@ function renderRestaurantes(lista) {
     
     const horario = document.createElement('p');
     horario.textContent = `Horario: ${restaurante.horario}`;
+
+    const reputacion = document.createElement('p');
+    reputacion.textContent = `Calificación: ${restaurante.reputacion} estrellas`;
     
     restaurantDiv.appendChild(nombre);
     restaurantDiv.appendChild(tipo);
     restaurantDiv.appendChild(horario);
-    
+    restaurantDiv.appendChild(reputacion);
+
     container.appendChild(restaurantDiv);
   });
 }
