714f9ba6a0f32f1279a7c45dd50f3054b25b7e6e
dg <danny.guamanl@gmail.com>
2025-05-01 12:45:11 -0500

    Incremento 1: PoC sobre tecnologías básicas del lado del cliente (HTML + CSS + JS)

---
+++ 1_Fundamentos/script.js
@@ -0,0 +1,31 @@
+function showMenu(menu) {
+
+    const menuTitle = document.getElementById('menuTitle');
+    const menuContent = document.getElementById('menuContent');
+    
+    // Cambiar el título según el menú seleccionado
+    menuTitle.textContent = `Has seleccionado: ${menu}`;
+  
+    // Cambiar el contenido según el menú seleccionado
+    switch(menu) {
+      
+      case 'Inicio':
+        menuContent.textContent = 'Bienvenido a la página de inicio de ñanEC.';
+        document.getElementById('intro').style.backgroundColor = '#f0f0f0';
+        break;
+      case 'Restaurantes':
+        menuContent.textContent = 'Explora los mejores restaurantes de San José.';
+        document.getElementById('intro').style.backgroundColor = '#ffcc99';
+        break;
+      case 'Recetas':
+        menuContent.textContent = 'Descubre recetas típicas de la región.';
+        document.getElementById('intro').style.backgroundColor = '#99ff99';
+        break;
+      case 'Eventos':
+        menuContent.textContent = 'Consulta los próximos eventos en San José.';
+        document.getElementById('intro').style.backgroundColor = '#99ccff';
+        break;
+    }
+        
+  }
+ 
