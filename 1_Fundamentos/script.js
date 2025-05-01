7c0b724a230ac4bbba2bd1892c4ce81afbbeb77e
LuisKan <luis.guerrero01@epn.edu.ec>
2025-05-01 14:51:07 -0500

    Actualizacion a las modificaciones relaizadas durante las 3 primeras semanas de clases

--- 1_Fundamentos/script.js
+++ 1_Fundamentos/script.js
@@ -1,17 +1,20 @@
-function showMenu(menu) {
 
+function showMenu(menu) {
+  /*console.log(document.getElementById('menuTitle'));
+  console.log(document.getElementById('menuContent'));
+  document.getElementById('menuTitle').style.backgroundColor = 'yellow';*/
     const menuTitle = document.getElementById('menuTitle');
     const menuContent = document.getElementById('menuContent');
     
-    // Cambiar el título según el menú seleccionado
+    // Cambiar el título según el menú seleccionado en el HTML
     menuTitle.textContent = `Has seleccionado: ${menu}`;
   
     // Cambiar el contenido según el menú seleccionado
     switch(menu) {
-      
       case 'Inicio':
-        menuContent.textContent = 'Bienvenido a la página de inicio de ñanEC.';
-        document.getElementById('intro').style.backgroundColor = '#f0f0f0';
+        menuContent.textContent = 'Bienvenido a la página de inicio de ñanEC.'; // Cambiar el contenido del menú según la opción seleccionada
+        document.getElementById('intro').style.backgroundColor = '#f0f0f0'; // Cambiar el color de fondo del elemento con id 'intro'
+        // getElementById acceder a un elemento del html por su id y cambiar el color de fondo(modicifar DOM)
         break;
       case 'Restaurantes':
         menuContent.textContent = 'Explora los mejores restaurantes de San José.';
@@ -26,6 +29,6 @@ function showMenu(menu) {
         document.getElementById('intro').style.backgroundColor = '#99ccff';
         break;
     }
-        
+  
   }
- 
+  
