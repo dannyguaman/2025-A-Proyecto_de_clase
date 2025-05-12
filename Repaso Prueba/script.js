f5ba89de4da4ee623d6fb61b037f89c4cb4286bc
Ssunsito <santiago.pila@epn.edu.ec>
2025-05-12 14:22:14 -0500

    aaa

---
+++ Repaso Prueba/script.js
@@ -0,0 +1,49 @@
+const html_icon =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png'
+const js_icon =
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Javascriptshield.svg/1200px-Javascript-shield.svg.png'
+const css_icon =
+    'https://i.pinimg.com/originals/eb/7e/20/eb7e20e646f5b7ec9ed4f8f78a5dee8f.png'
+const node_icon = 'https://icon-library.com/images/nodejs-icon/nodejsicon-17.jpg'
+const mongo_icon = 'https://cdn.iconicons.com/icons2/2415/PNG/512/mongodb_plain_wordmark_logo_icon_146423.png'
+const developerList = [
+    {
+        name: 'Carla',
+        photo:
+            'https://i.pinimg.com/236x/6d/5e/38/6d5e38d19bf4c0c9554b1e6beab75952.jpg',
+        bio: 'Hola, soy desarrolladora front-end desde hace 5 años...',
+        languages: {
+            html: true,
+            javascript: true,
+            css: true,
+            node: false,
+            mongodb: false
+        }
+    },
+    {
+        name: 'Erick',
+        photo:
+            'https://i.pinimg.com/564x/e9/57/2a/e9572a70726980ed5445c02e1058760b.jpg',
+        bio: 'Hi, I am back-end developer from ten years ago, and ...',
+        languages: {
+            html: true,
+            javascript: true,
+            css: false,
+            node: true,
+            mongodb: true
+        }
+    },
+    {
+        name: 'Maria',
+        photo:
+            'https://i.pinimg.com/564x/ed/be/19/edbe19b1fd4866b2d458aaabf8c02073.jpg',
+        bio: 'Hola, soy DevOps en Facebook ...',
+        languages: {
+            html: false,
+            javascript: false,
+            css: false,
+            node: true,
+            mongodb: true
+        }
+    },
+];
