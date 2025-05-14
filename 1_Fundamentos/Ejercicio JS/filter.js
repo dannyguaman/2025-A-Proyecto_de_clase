d03f64235aea3563e4fdb4bee5b63a3b916f4692
Jose Condor <josecondor02@hotmail.com>
2025-05-14 16:02:22 -0500

    Add execution instructions and enhance comments in filter.js and map.js

--- 1_Fundamentos/Ejercicio JS/filter.js
+++ 1_Fundamentos/Ejercicio JS/filter.js
@@ -7,18 +7,15 @@ Luego, ejecuta el siguiente comando en la terminal: node .\nombre_del_archivo.js
 
 /* ----- FUNCIONES DE ORDEN SUPERIOR ---- */
 // una funcion que recibe otra función como argumento o devuelve una función como resultado se considera de orden superior
-/* ----- .map() ---- */
+/* ----- .filter() ---- */
 // Definimos un arreglo con diferentes edades
 var edades = [12, 15, 18, 20, 25, 30, 35, 40];
 
-// Usamos el método map para recorrer cada elemento del arreglo
-edades.map(function (edad) {
+// usamos el método filter para recorrer cada elemento del arreglo
+edades.filter(function (edad) {
     // Si la edad es mayor o igual a 18
     if (edad >= 18) {
         // Imprime que es mayor de edad
         console.log(edad + " es mayor de edad");
-    } else {
-        // Imprime que es menor de edad
-        console.log(edad + " es menor de edad");
-    }
+    } 
 });
