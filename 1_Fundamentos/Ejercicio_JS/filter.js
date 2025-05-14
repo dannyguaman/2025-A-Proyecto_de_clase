dfeb418245fefa424cb43d60e7009e370e435427
cedamoch <cedamoch@gmail.com>
2025-05-14 16:01:58 -0500

    Add filter and map functions to process arrays

---
+++ 1_Fundamentos/Ejercicio_JS/filter.js
@@ -0,0 +1,3 @@
+var a = [1, 2, 3, 4, 5];
+var numeros = a.filter(function (x) { return x % 2 == 0; })
+console.log(numeros);
---
+++ 1_Fundamentos/Ejercicio_JS/map.js
@@ -0,0 +1,10 @@
+var edades = [20, 30, 34, 8];
+var edadesMapeadas = edades.map(function (edad) {
+    if (edad >= 18) {
+        return edad;
+    }else {
+        return edad + 4;
+    }
+});
+console.log(edadesMapeadas);
+
