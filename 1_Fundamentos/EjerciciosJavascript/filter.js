cc579fef0a1f8c1b9721e4f485e98e16655e25d0
Jean <jean.cardoso@epn.edu.ec>
2025-05-14 16:02:46 -0500

    Javascript

---
+++ 1_Fundamentos/EjerciciosJavascript/filter.js
@@ -0,0 +1,7 @@
+var a = [20,38,8];  
+var x = 0;
+a.filter(function (x) { return x % 2 == 0; })
+    if(x>=18)
+        x = x + 4;  
+else
+console.log(x); // [2, 4, 6, 8, 10]
---
+++ 1_Fundamentos/EjerciciosJavascript/map.js
@@ -0,0 +1,12 @@
+
+var edades = [20,38,8];
+edades.map(function (edad) {
+    if (edad < 18) {
+        edad = edad + 4;
+    } return edad;
+
+    
+}
+)
+
+console.log(edad); // [20, 38, 8]
