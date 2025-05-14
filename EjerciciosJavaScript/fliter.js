e6899fdcce52cdce0a46c86f1b40e6aac023feb4
LuisKan <luis.guerrero01@epn.edu.ec>
2025-05-14 16:02:27 -0500

    Uso de filter y map

---
+++ EjerciciosJavaScript/fliter.js
@@ -0,0 +1,7 @@
+//var a = [1, 2, 3, 4, 5];
+//var pares = a.filter(function (x) { return x % 2 == 0; });
+//console.log(pares);
+ var edades = [20, 30, 34, 8];
+ var mayores1Aaños = edades.filter(function (edad) { return edad >= 18; });
+    console.log(mayores1Aaños);
+    
---
+++ EjerciciosJavaScript/map.js
@@ -0,0 +1,3 @@
+var edades = [20, 30, 34, 8];
+edades.map(function (edad) { if (edad <= 18) { return edad + 4 } return edad });
+console.log(edades);
