c741de7e62e12990015de68a026bcbb37659e1af
Mathew EPN <mathew.gutierrez@epn.edu.ec>
2025-05-15 14:57:11 -0500

    Refactor HTML structure and JavaScript functions for age validation and greeting

--- 1_Fundamentos/Ejercio JS/Eventos/script.js
+++ 1_Fundamentos/Ejercio JS/Eventos/script.js
@@ -1,4 +1,14 @@
-() => {
+const saludar = () => {
     var name = document.getElementById("name").value; 
     alert("Hi, " + name + "!");
+}
+
+function validadEdad() {
+    var edad = document.getElementById("age").value;
+    var nombre = document.getElementById("name").value;
+    if (edad < 21) {
+        alert(nombre+", you can't drink");
+    } else {
+        alert(nombre+", you can drink"); 
+    }
 }
