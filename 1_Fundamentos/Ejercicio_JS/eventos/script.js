a781e3654c1b2df04093b2e54865257507185ac6
Ssunsito <santiago.pila@epn.edu.ec>
2025-05-15 15:00:20 -0500

    Ejercicio 2 JS

--- 1_Fundamentos/Ejercicio_JS/eventos/script.js
+++ 1_Fundamentos/Ejercicio_JS/eventos/script.js
@@ -1,6 +1,14 @@
-function myFunction() {
-    alert("Hello, world!");
+const welcome = () => {
+    var name = document.getElementById("name").value;
+    alert("Hi, " + name + "!");
 }
-function welcome() {
-    var name = document.getElementById("name").value; alert("Hi, " + name + "!");
+
+function checkage() {
+    var name = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    if (age >= 18) {
+        alert("Hola, " + name + "! Eres mayor de edad.");
+    } else {
+        alert("Hola, " + name + "! Eres menor de edad.");
+    }
 }
