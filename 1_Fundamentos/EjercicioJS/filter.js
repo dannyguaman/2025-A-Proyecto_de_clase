19f4fba8613f5ab1aff616d4dd50eaa64a77f60a
user.name <oscar.sala@epn.edu.ec>
2025-05-15 14:12:12 -0500

    Uso de map y filter

---
+++ 1_Fundamentos/EjercicioJS/filter.js
@@ -0,0 +1,4 @@
+var a = [1, 2, 3, 4, 5];
+var edadesPares = a.filter(function(x) { return x % 2 == 0; })
+
+console.log(edadesPares); 
---
+++ 1_Fundamentos/EjercicioJS/map.js
@@ -0,0 +1,11 @@
+var edades = [20,30,34,8]
+
+var edadesMapeadas = edades.map(function(edad){
+    if(edad >= 18){
+        return edad;
+    } else{
+        return edad + 4;
+    } 
+})
+
+console.log(edadesMapeadas);
