3b769ec4dbee3f283cc146a2a9b5b4682214876a
Ssunsito <santiago.pila@epn.edu.ec>
2025-05-21 15:47:05 -0500

    Reactjs

---
+++ nanec/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Restaurante from './Componentes/Restaurante';
+
+function App() {
+  const restaurantes = [{nombre: "Restaurante El Buen Sabor", direccion: "Av. 10 de Agosto", tipo: "Ecuatoriana", imagen: ""},
+                    {nombre: "Cafetería La Unión", direccion: "10 de Agosto", tipo: "Cafetería"},
+                    {nombre: "La Mexicana", direccion: "La Prensa y La Católica", tipo: "Mexicana"}
+  ]
+  return (
+    <div className="App">
+      {
+        restaurantes.map((restaurante, index) => (
+          <Restaurante 
+            key={index} 
+            nombre={restaurante.nombre} 
+            direccion={restaurante.direccion} 
+            tipo={restaurante.tipo} 
+            imagen={restaurante.imagen} 
+          />
+        ))
+      }
+    </div>
+  );
+}
+
+export default App;
+
