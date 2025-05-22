546d7634ac12ed923f4df0b5c573433405f2adef
Ssunsito <santiago.pila@epn.edu.ec>
2025-05-22 15:33:11 -0500

    Importación nuevo proyecto

---
+++ nanec_funcional1/componentes/restaurante.jsx
@@ -0,0 +1,30 @@
+// React solo es una librería de JavaScript para construir interfaces de usuario, 
+// no es un framework completo como Angular o Vue.js. 
+// Por lo tanto, no tiene un concepto de "módulos" como tal,
+//  pero puedes organizar tu código en componentes y archivos para mantenerlo modular y reutilizable.
+import React from "react";
+import "./Restaurante.css";
+
+function Restaurante(props) {
+    const { nombre, direccion, tipo } = props;
+    
+    const [likes, setLikes] = useState(0);
+
+    const handlerLike = () => {
+        setLikes(likes + 1);
+    };
+
+    return (
+        <div>
+            <h1>{nombre}</h1>
+            <h2>{direccion}</h2>
+            <h3>{tipo}</h3>
+            <h4>{likes}</h4>
+            <button onClick={handlerLike}>Like</button>
+        </div>
+    );
+}
+
+export default Restaurante;
+
+
